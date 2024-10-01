e3832c4551083fb423e765f4ee2221a10803fd84
Ivan Matev <Ivan.Matev@Seattlecolleges.edu>
2024-10-01 08:16:00 -0700

    Added a compare function within the sort function

--- Week 2 Assignments/Assignment 3.js
+++ Week 2 Assignments/Assignment 3.js
@@ -3,8 +3,12 @@ const letters = ["e", "c", "a", "b", "d"];
 // print the unsorted array
 console.log(letters);
 // sort the original letters array
-letters.sort((c1, c2) => c1.localeCompare(c2));
+letters.sort(compare);
 // make a copy of the letters array
 const sortedArray = [...letters];
 // print the new array
 console.log(sortedArray);
+
+function compare(a, b) {
+  return a.localeCompare(b);
+}
