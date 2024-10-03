aecf5e5109d67609509c2fecc4f58611a645d053
Ivan Matev <Ivan.Matev@Seattlecolleges.edu>
2024-10-02 18:12:42 -0700

    Week 3 assignment 1 complete

---
+++ Week 3 Assignments/Assignment 1.js
@@ -0,0 +1,37 @@
+// All of Assignment 1 should be done in Node
+// --------------------
+// Part 1
+1 + 3;
+// Output: 4
+1 + 2 * 3 - 4;
+// Output: 3
+// --------------------
+// Part 2
+x = 0;
+do {
+  x++;
+  console.log("x: " + x);
+} while (x < 5);
+/*
+Output:
+x: 1
+x: 2
+x: 3
+x: 4
+x: 5
+*/
+// --------------------
+// Part 3
+do {
+  x++;
+  console.log("x: " + x);
+} while (x < 5);
+x: 1;
+x: 2;
+x: 3;
+x: 4;
+x: 5;
+/*
+x: 6
+5
+*/
